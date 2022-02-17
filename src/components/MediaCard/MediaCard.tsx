23f24307e248bec69ba2282e5be8279890dfeca5
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2022-02-18 01:03:50 +0300

    stable update.

--- src/components/MediaCard/MediaCard.tsx
+++ src/components/MediaCard/MediaCard.tsx
@@ -12,8 +12,8 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import BookmarkAdd from '@mui/icons-material/BookmarkAdd';
-import ThumbDown from '@mui/icons-material/ThumbDown';
-import ThumbUp from '@mui/icons-material/ThumbUp';
+import ListOutlined from '@mui/icons-material/ListOutlined';
+import VideoLibrary from '@mui/icons-material/VideoLibrary';
 
 import LinkOutlined from '@mui/icons-material/LinkOutlined';
 
@@ -29,109 +29,74 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 interface MediaCardProps {
-  channelTitle: string, 
-  title: string, 
-  publishedAt: string, 
-  description: string, 
+  channelTitle: string,
+  title: string,
+  publishedAt: string,
+  description: string,
   imageUrl: string,
   channelId: string,
+  videoId: string,
 }
 
-const MediaCard = ({channelTitle, title, publishedAt, description, imageUrl, channelId}: MediaCardProps) => {
-    const theme = useTheme();
+const MediaCard = ({ channelTitle, title, publishedAt, description, imageUrl, channelId, videoId }: MediaCardProps) => {
+  const theme = useTheme();
 
 
-    return (
-        
+  return (
 
-        
 
-<>
 
-<Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={channelTitle}
-        subheader={new Date(publishedAt).toDateString()}
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={imageUrl}
-        alt=""
-      />
-      <CardContent>
-      <Typography variant="body1" color="text.primary">
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          aria-label="show more"
-        >
-          <LinkOutlined />
-        </IconButton>
-      </CardActions>
 
-    </Card>
-{/* <Card sx={{ display: 'flex', width: '450px' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <CardContent sx={{ flex: '1 0 auto' }}>
-              <Typography component="div" variant="h5">
-                {channelTitle}
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary" component="div">
-                {title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+    <>
+
+      <Card sx={{ maxHeight: 600 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              {channelTitle.charAt(0)}
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={channelTitle}
+          subheader={new Date(publishedAt).toDateString()}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={imageUrl}
+          alt=""
+        />
+        <CardContent>
+          <Typography variant="body1" color="text.primary">
+            {title}
           </Typography>
-            </CardContent>
-            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
 
-              <IconButton aria-label={theme.direction === 'rtl' ? "thumbdown" : 'thumbup'}>
-                {theme.direction === 'rtl' ? <ThumbDown /> : <ThumbUp />}
-              </IconButton>
-              <IconButton aria-label={theme.direction === 'rtl' ? "thumbup" : 'thumbdown'}>
-                {theme.direction === 'rtl' ? <ThumbUp /> : <ThumbDown />}
-              </IconButton>
+          <IconButton aria-label="view" href={`https://youtube.com/watch?v=${videoId}`}>
+            <VideoLibrary  />
+          </IconButton>
+          <IconButton
+            aria-label="see more"
+            href={`https://youtube.com/channel/${channelId}`}
+          >
+            <ListOutlined  />
+          </IconButton>
+        </CardActions>
 
-              <IconButton aria-label="bookmark">
-                { <BookmarkAdd /> }
-              </IconButton>
-
-            </Box>
-          </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: 151 }}
-            image={imageUrl}
-            alt="Live from space album cover"
-          />
-        </Card> */}
-</>
+      </Card>
+      
+    </>
 
 
-      );
+  );
 }
 
 export default MediaCard;
--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchInput.css";
 
 import Box from '@mui/material/Box';
@@ -14,201 +14,23 @@ import Divider from '@mui/material/Divider';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import DirectionsIcon from '@mui/icons-material/Directions';
+import { experimentalStyled as styled } from '@mui/material/styles';
 
+import Grid from '@mui/material/Grid';
 import MediaCard from '../MediaCard/MediaCard';
 
 function SearchInput() {
 
     const [searchText, setSearchText] = useState('');
-    const [searchResults, setSearchResults] = useState({
-        "kind": "youtube#searchListResponse",
-        "etag": "CShPe4_GsJiHSTFqN9AOMaaDZ1g",
-        "nextPageToken": "CAUQAA",
-        "regionCode": "KE",
-        "pageInfo": {
-            "totalResults": 1000000,
-            "resultsPerPage": 5
-        },
-        "items": [
-            {
-                "kind": "youtube#searchResult",
-                "etag": "yTD7130Y10uXE4YW4F4xvmTuKIs",
-                "id": {
-                    "kind": "youtube#video",
-                    "videoId": "x2p6OEXUw_w"
-                },
-                "snippet": {
-                    "publishedAt": "2022-02-16T14:49:06Z",
-                    "channelId": "UCWj10BWkvjXBS_MZsTbylRw",
-                    "title": "Bubble Gum VS Chocolate Food Challenge | Eating Only Giant Sweets For 24 Hours by RATATA COOL",
-                    "description": "Subscribe for RATATA COOL: https://www.youtube.com/channel/UCWj10BWkvjXBS_MZsTbylRw?sub_confirmation=1 Guys, we ...",
-                    "thumbnails": {
-                        "default": {
-                            "url": "https://i.ytimg.com/vi/x2p6OEXUw_w/default_live.jpg",
-                            "width": 120,
-                            "height": 90
-                        },
-                        "medium": {
-                            "url": "https://i.ytimg.com/vi/x2p6OEXUw_w/mqdefault_live.jpg",
-                            "width": 320,
-                            "height": 180
-                        },
-                        "high": {
-                            "url": "https://i.ytimg.com/vi/x2p6OEXUw_w/hqdefault_live.jpg",
-                            "width": 480,
-                            "height": 360
-                        }
-                    },
-                    "channelTitle": "RATATA COOL!",
-                    "liveBroadcastContent": "live",
-                    "publishTime": "2022-02-16T14:49:06Z"
-                }
-            },
-            {
-                "kind": "youtube#searchResult",
-                "etag": "ssEaR02GA5XT7BDS6k7jYamy6aY",
-                "id": {
-                    "kind": "youtube#video",
-                    "videoId": "5iq3hXmTyAs"
-                },
-                "snippet": {
-                    "publishedAt": "2022-02-16T14:46:55Z",
-                    "channelId": "UC1I_DaL4stfZi03ozQEQ1VQ",
-                    "title": "EATING ONLY ONE COLOR FOOD FOR 24 HOURS | Red VS Blue Cake Decorating! Jelly VS Chocolate by RATATA",
-                    "description": "Subscribe for RATATA: https://www.youtube.com/channel/UC1I_DaL4stfZi03ozQEQ1VQ?sub_confirmation=1 New! Now, for 24 ...",
-                    "thumbnails": {
-                        "default": {
-                            "url": "https://i.ytimg.com/vi/5iq3hXmTyAs/default_live.jpg",
-                            "width": 120,
-                            "height": 90
-                        },
-                        "medium": {
-                            "url": "https://i.ytimg.com/vi/5iq3hXmTyAs/mqdefault_live.jpg",
-                            "width": 320,
-                            "height": 180
-                        },
-                        "high": {
-                            "url": "https://i.ytimg.com/vi/5iq3hXmTyAs/hqdefault_live.jpg",
-                            "width": 480,
-                            "height": 360
-                        }
-                    },
-                    "channelTitle": "RATATA",
-                    "liveBroadcastContent": "live",
-                    "publishTime": "2022-02-16T14:46:55Z"
-                }
-            },
-            {
-                "kind": "youtube#searchResult",
-                "etag": "yPXEROG-hRfHEn9FCD8_wFhTEzY",
-                "id": {
-                    "kind": "youtube#video",
-                    "videoId": "YUDia01zoRg"
-                },
-                "snippet": {
-                    "publishedAt": "2022-02-16T20:31:22Z",
-                    "channelId": "UCfw8x3VR-ElcaWW2Tg_jgSA",
-                    "title": "Last to Leave Vending Machine Wins $10,000 - Rebecca Zamolo",
-                    "description": "Bigs vs Minis in Last to leave vending machine wins. It all started when Rebecca Zamolo created \"Third Wheeling My Best Friends ...",
-                    "thumbnails": {
-                        "default": {
-                            "url": "https://i.ytimg.com/vi/YUDia01zoRg/default.jpg",
-                            "width": 120,
-                            "height": 90
-                        },
-                        "medium": {
-                            "url": "https://i.ytimg.com/vi/YUDia01zoRg/mqdefault.jpg",
-                            "width": 320,
-                            "height": 180
-                        },
-                        "high": {
-                            "url": "https://i.ytimg.com/vi/YUDia01zoRg/hqdefault.jpg",
-                            "width": 480,
-                            "height": 360
-                        }
-                    },
-                    "channelTitle": "Rebecca Zamolo",
-                    "liveBroadcastContent": "none",
-                    "publishTime": "2022-02-16T20:31:22Z"
-                }
-            },
-            {
-                "kind": "youtube#searchResult",
-                "etag": "BbJRkMR9yNZKxEUanjj9vWmnwck",
-                "id": {
-                    "kind": "youtube#video",
-                    "videoId": "nCL6AYspuvs"
-                },
-                "snippet": {
-                    "publishedAt": "2022-02-16T07:44:26Z",
-                    "channelId": "UCCcprrrcbdaj14kYPjcbj9w",
-                    "title": "YouTube-шоу «Гордон» №4",
-                    "description": "Стань слушателем Академии Дмитрия Гордона! Стань успешным и богатым! Проходи по ссылке: https://bit.ly/3cXrAdF ...",
-                    "thumbnails": {
-                        "default": {
-                            "url": "https://i.ytimg.com/vi/nCL6AYspuvs/default_live.jpg",
-                            "width": 120,
-                            "height": 90
-                        },
-                        "medium": {
-                            "url": "https://i.ytimg.com/vi/nCL6AYspuvs/mqdefault_live.jpg",
-                            "width": 320,
-                            "height": 180
-                        },
-                        "high": {
-                            "url": "https://i.ytimg.com/vi/nCL6AYspuvs/hqdefault_live.jpg",
-                            "width": 480,
-                            "height": 360
-                        }
-                    },
-                    "channelTitle": "В гостях у Гордона",
-                    "liveBroadcastContent": "upcoming",
-                    "publishTime": "2022-02-16T07:44:26Z"
-                }
-            }, {
-                "kind": "youtube#searchResult",
-                "etag": "WmNyisHhyeM5V3DJMb3g6XoGdG8",
-                "id": {
-                    "kind": "youtube#video",
-                    "videoId": "SZbUPoe70Bo"
-                },
-                "snippet": {
-                    "publishedAt": "2022-02-16T21:00:08Z",
-                    "channelId": "UC0otZdGYsA9KqVKAcn2peQA",
-                    "title": "MOTIVO DE NÃO POSTAR MAIS VLOGS DE ROTINA NO YOUTUBE - Família Brancoala",
-                    "description": "No daily vlog de hoje eu conto o motivo de não estar postando mais vlogs de rotina aqui no canal Brancoala. ✓ CAMISETAS ...",
-                    "thumbnails": {
-                        "default": {
-                            "url": "https://i.ytimg.com/vi/SZbUPoe70Bo/default.jpg",
-                            "width": 120,
-                            "height": 90
-                        },
-                        "medium": {
-                            "url": "https://i.ytimg.com/vi/SZbUPoe70Bo/mqdefault.jpg",
-                            "width": 320,
-                            "height": 180
-                        },
-                        "high": {
-                            "url": "https://i.ytimg.com/vi/SZbUPoe70Bo/hqdefault.jpg",
-                            "width": 480,
-                            "height": 360
-                        }
-                    },
-                    "channelTitle": "Brancoala",
-                    "liveBroadcastContent": "none",
-                    "publishTime": "2022-02-16T21:00:08Z"
-                }
-            }
-        ]
-    });
+    const [searchResults, setSearchResults] = useState<any>(null);
 
-    const handleChange = (event : any) => { // <SyntheticBaseEvent>
+    const handleChange = (event: any) => { // <SyntheticBaseEvent>
 
         setSearchText(event.target.value)
     };
 
 
-    const handleSubmit = (event : any) => {
+    const handleSubmit = (event: any) => {
         console.log('submitting');
 
         event.preventDefault();
@@ -219,7 +41,7 @@ function SearchInput() {
 
     const askYouTube = async () => {
         try { // todo, create baseUrl and use dynamically
-            let res = await fetch(`http://localhost:4005/search/?queryString="${searchText}`, {
+            let res = await fetch(`http://localhost:4005/api/search/?queryString="${searchText}`, {
                 method: "GET",
                 // body: JSON.stringify({ // only for 'POST'
                 //     query: searchText,
@@ -228,6 +50,8 @@ function SearchInput() {
 
             let resJson = await res.json();
             console.log('result', resJson);
+
+            setSearchResults(resJson)
 
             if (res.status === 200) {
                 // show message to user
@@ -256,32 +80,46 @@ function SearchInput() {
                 onSubmit={handleSubmit}>
 
                 <InputBase sx={
-                        {
-                            ml: 1,
-                            flex: 1
-                        }
+                    {
+                        ml: 1,
+                        flex: 1
                     }
+                }
                     placeholder="Search YouTube :)"
                     inputProps={
-                        {'aria-label': 'search youtube'}
+                        { 'aria-label': 'search youtube' }
                     }
-                    onChange={handleChange}/>
+                    onChange={handleChange} />
                 <IconButton sx={
-                        {p: '10px'}
-                    }
+                    { p: '10px' }
+                }
                     aria-label="search"
                     onClick={handleSubmit}>
-                    <SearchIcon/>
+                    <SearchIcon />
                 </IconButton>
 
 
             </Paper>
 
-            {
-            searchResults.items.map((item, index) => (
-              <MediaCard key={item.etag} channelTitle={item.snippet.channelTitle} title={item.snippet.title} publishedAt={item.snippet.publishedAt} description={item.snippet.description} imageUrl={item.snippet.thumbnails.high.url} channelId={item.snippet.channelId} />
-    ))
-        } </>
+            <Divider>Search Results</Divider>
+
+
+            <section className="result-wrapper">
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}  >
+                    {
+                        searchResults && searchResults?.items.length > 0 && searchResults.items.map((item: any, index: number) => (
+                            <Grid item xs={6} sm={4} md={3} key={index}>
+                                <MediaCard key={item.etag} videoId={item.id.videoId} channelTitle={item.snippet.channelTitle} title={item.snippet.title} publishedAt={item.snippet.publishedAt} description={item.snippet.description} imageUrl={item.snippet.thumbnails.high.url} channelId={item.snippet.channelId} />
+                            </Grid>
+
+                        ))
+                    }
+                </Grid>
+            </Box>
+            </section>
+
+        </>
 
     );
 }
